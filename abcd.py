618acb204170ab4f074f52834c657f9a4e7321e7
Francois Schwarzentruber <fschwarz@schumann.irisa.fr>
2022-10-23 11:20:54 +0200

    multiple voices, multiple staffs

--- abcd.py
+++ abcd.py
@@ -1,4 +1,25 @@
+from re import S
+
+
 filename = "example.abcd"
+
+
+
+
+
+class Score:
+  def __init__(self):
+    self.staffs = []
+
+  def add(self, istaff, ivoice, data):
+    if len(self.staffs) - 1 < istaff:
+      self.staffs.append([])
+
+    if len(self.staffs[istaff]) - 1 < ivoice:
+      self.staffs[istaff].append("")
+
+    self.staffs[istaff][ivoice] += data
+
 
 
 def toScore(lines):
@@ -7,30 +28,29 @@ def toScore(lines):
   :return an array that represents the score. That array is an array of measures.
   Each measure is an array of staffs. Each staffs is an array of voices.
   """
-  measures = []
-  measure = []
-  staff = []
+
+  score = Score()
+
+  istaff = 0
+  ivoice = 0
+  sthAdded = False
+
   for line in lines:
-    if line == "\n" or line == "":
-      if len(staff) > 0:
-       measure.append(staff)
-      staff = []
-    elif line == "|\n":
-      if len(staff) > 0:
-        measure.append(staff)
-        staff = []
-      measures.append(measure)
-      measure = []
+    if line == "":
+      if sthAdded:
+          istaff += 1
+          ivoice = 0
+    elif line == "|":
+      sthAdded = False
+      istaff = 0
+      ivoice = 0
     else:
-      staff.append(line)
-  
-  if len(staff) > 0:
-    measure.append(staff)
-    staff = []
-  if len(measure) > 0:
-    measures.append(measure)
-    measure = []
-  return measures
+      score.add(istaff, ivoice, line)
+      sthAdded = True
+      ivoice += 1
+
+  return score
+
 
 
 def toLilypond(lines):
@@ -38,10 +58,33 @@ def toLilypond(lines):
      :param lines in the .abcd format
      :return lines in the lilypond format
      """
-     measures = toScore(lines)
-     #print(measures)
-     voice1 = ' '.join(map(lambda m: m[0][0],measures))
-     voice2 = ' '.join(map(lambda m: m[1][0],measures))
+
+
+
+     def toLilypondVoice(voice):
+          return '\\new Voice  = "miaou" { ' + voice.replace("𝄢", "\\clef bass").replace("𝄞", "\\clef treble") + "} \n"
+
+
+     def toLilypondStaff(staff):
+      s = "\\new Staff <<\n"
+      for voice in staff:
+            s += toLilypondVoice(voice) + "\n"
+      s += ">>\n"
+
+      return s
+
+     def toLilypondScore(score):
+      s = "\\new Score <<\n"
+      for staff in score.staffs:
+            s += toLilypondStaff(staff) + "\n"
+      s += ">>"
+      return s
+
+
+     score = toScore(lines)
+     print(score.staffs)
+
+     return toLilypondScore(score)
      lilyLines = """upper = \\relative c'' {
   \\clef treble
   \\key c \\major
@@ -78,10 +121,8 @@ def abcdFileToPdf(filename):
   :effect create a pdf
   """
   file = open(filename, "r")
-
-  lines = file.readlines()
+  lines = map(lambda line: line.replace("\n", "") , file.readlines())
   file.close()
-
 
   lilePondCode = toLilypond(lines)
 
